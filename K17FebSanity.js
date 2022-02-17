0b736045ac9a6ba11420fcdf4f3b90eb8115e0eb
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2022-02-17 19:35:37 +0530

    m

--- K17FebSanity.js
+++ K17FebSanity.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
@@ -8,12 +9,23 @@ exports.handler = async (event) => {
             UserPoolId: "us-east-1_HdYJb7Znp",
             Limit: 10
         }).promise();
+        //console.log(data);
+
+    } catch (err) {
+        //console.log(err);
+        // error handling goes here
+    };
+    //console.log("test");
+    try {
+        let data = await s3.listObjects({
+            Bucket: "cloud9-ktest",
+            MaxKeys: 10
+        }).promise();
         console.log(data);
 
     } catch (err) {
-        console.log(err);
+        //console.log(err);
         // error handling goes here
     };
-    console.log("test");
     return { "message": "Successfully executed" };
 };
